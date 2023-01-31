ddbefd1258cb877714943e4b9108449010033e21
FJohnHRoss <rossjohn936@gmail.com>
2023-01-31 15:23:41 -0800

    login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,17 +2,53 @@ const User = require('../models/user.model')
 const myFirstSecret = process.env.SECRET_KEY;
 
 
-const register = (req, res) => {
+module.exports.register = (req, res) => {
   User.create(req.body)
     .then(user => {
+      const userToken = jwt.sign({
+        id: user._id
+      }, myFirstSecret);
+
       res
-        .cookie(
-          "usertoken",
-          jwt.sign({ id: user._id }, myFirstSecret),
-          {
-            httpOnly: true,
-          })
-        .json({ msg: "success!" })
-        .catch(err => res.json(err));
+        .cookie("usertoken", userToken, secret, {
+          httpOnly: true
+        })
+        .json({ msg: "success!", user: user });
     })
+    .catch(err => res.json(err));
+}
+
+module.exports.login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (user === null) {
+    // email not found in users collection
+    return res.sendStatus(400);
+  }
+
+  // if we made it this far, we found a user with this email address
+  // let's compare the supplied password to the hashed password in the database
+  const correctPassword = await bcrypt.compare(req.body.password, user.password);
+
+  if (!correctPassword) {
+    // password wasn't a match!
+    return res.sendStatus(400);
+  }
+
+  // if we made it this far, the password was correct
+  const userToken = jwt.sign({
+    id: user._id
+  }, myFirstSecret);
+
+  // note that the response object allows chained calls to cookie and json
+  res
+    .cookie("usertoken", userToken, secret, {
+      httpOnly: true
+    })
+    .json({ msg: "success!" });
+}
+
+module.exports.logout = (req, res) => {
+  res.clearCookie('usertoken');
+  res.sendStatus(200);
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,9 @@
 const UserController = require('../controllers/user.controller')
+const { authenticate } = require('../config/jwt.config');
 
 module.exports = function(app) {
-  
+  app.post('/api/register', UserController.register);
+  app.post("/api/login", UserController.login);
+  // this route now has to be authenticated
+  // app.get("/api/users", authenticate, UserController.get);
 }
