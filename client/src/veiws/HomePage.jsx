a112659c1d703ba09a8fe157ffac44ca75d44935
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-02-01 12:35:51 -0800

    budget banner

--- client/src/veiws/HomePage.jsx
+++ client/src/veiws/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import logo from '../images/budget-banner.png';
 
 
 const HomePage = (props) => {
@@ -8,8 +9,10 @@ const HomePage = (props) => {
       <NavBar theme={props.theme} setTheme={props.setTheme}/>
 
       <h1 className='text-center'>Budget App</h1>
+      <img src={logo} alt={"logo"} />
     </>
   )
 }
 
 export default HomePage
+
