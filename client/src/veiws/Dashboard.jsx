cae66a6c6f603b78e81fb7e6489c3006b610cfc2
FJohnHRoss <rossjohn936@gmail.com>
2023-02-01 13:24:05 -0800

    Update Dashboard.jsx

--- client/src/veiws/Dashboard.jsx
+++ client/src/veiws/Dashboard.jsx
@@ -20,7 +20,11 @@ const Dashboard = (props) => {
   return (
     <>
       <NavBar theme={props.theme} setTheme={props.setTheme} />
-      <p>{user ? user.email : ""}</p>
+      <p>{user ? 
+      <>
+      <p>{user.email}</p>
+        <p>{user.phone}</p>
+      </> : ""}</p>
     </>
   )
 }
