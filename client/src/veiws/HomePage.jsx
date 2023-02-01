66af7af3e71d9d51f7c1b16b55ad6ebbe971c006
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-02-01 14:36:23 -0800

    banner images

--- client/src/veiws/HomePage.jsx
+++ client/src/veiws/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import logo from '../images/budget-banner.png';
+import logo2 from '../images/Balances.png';
+import logo3 from '../images/Bills.png';
+import logo4 from '../images/Budget.png';
 
 
 const HomePage = (props) => {
@@ -8,8 +11,15 @@ const HomePage = (props) => {
     <>
       <NavBar theme={props.theme} setTheme={props.setTheme}/>
 
-      <h1 className='text-center'>Budget App</h1>
+      <h1 className='text-center'>Budget Guard</h1>
       <img src={logo} alt={"logo"} />
+      <div>
+      </div>
+      <div>
+        <img src={logo2} alt={"logo2"} />
+        <img src={logo3} alt={"logo3"} />
+        <img src={logo4} alt={"logo4"} />
+      </div>
     </>
   )
 }
