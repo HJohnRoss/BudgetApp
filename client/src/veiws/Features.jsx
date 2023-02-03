bf7ab810a5207e96d50bd41c3d39625fb0428258
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-02-02 21:49:26 -0800

    features

---
+++ client/src/veiws/Features.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import feat_1 from '../images/feat-1.png';
+
+const Features = (props) => {
+
+  return (
+
+    <div>
+
+      <div>
+        <div>
+          <h3>Never miss a payment. Use our bills and subscription tracking tool</h3>
+          <p>Know what you're paying for and when. Define bills' deadlines and get notifications
+            from our app - never miss a single due date to avoid issues. No more manual work.
+            Everything you need to reach financial wellness is wrapped out in our monthly bill organizer.
+          </p>
+        </div>
+        <img src={feat_1} alt={"feat_1"} />
+      </div>
+
+      <div>
+
+      </div>
+
+
+
+    </div>
+
+  )
+
+}
