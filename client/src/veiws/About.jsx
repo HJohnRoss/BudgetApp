a9fa654f2dde4bf4393cbbb901c73bd2faa1e979
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-02-02 18:12:50 -0800

    about page added

---
+++ client/src/veiws/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const About = (props) => {
+
+
+  return (
+    <div>
+      <label>Name:</label>
+      <input type="text" />
+    </div>
+  )
+
+}
